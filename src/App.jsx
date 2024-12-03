ee7b192176923c5f2d0ed19f2a7aa80313825bcf
Labeebct <ctlabeebthaliyil@gmail.com>
2024-12-03 08:11:01 +0530

    deployment error made changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Layout from "./Layout/layout";
+import Layout from "./layout/Layout";
 
 //Importing all pages
 import Community from "./pages/Community";
